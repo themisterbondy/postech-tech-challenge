a8ac84339f256825cef2dea618c8435a6d537b00
Alexandre Nascimento <alexandre.n@outlook.com>
2024-07-19 01:21:44 -0300

    Add ProductRepository and IProductRepository
    Defined ProductRepository and its interface IProductRepository. This includes methods for creating, updating, deleting, and finding products by ID and category. This contributes to the functionality of the product management feature in the API.

---
+++ src/PosTech.MyFood.WebApi/Features/Products/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using PosTech.MyFood.Features.Products.Entities;
+
+namespace PosTech.MyFood.Features.Products.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product> FindByIdAsync(ProductId id, CancellationToken cancellationToken);
+    Task<List<Product>> FindByCategoryAsync(ProductCategory? category, CancellationToken cancellationToken);
+    Task<Product> CreateAsync(Product product, CancellationToken cancellationToken);
+    Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken);
+    Task DeleteAsync(Product product, CancellationToken cancellationToken);
+}
