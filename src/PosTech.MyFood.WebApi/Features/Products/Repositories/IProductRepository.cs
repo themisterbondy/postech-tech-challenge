0f34fb3f177ba02302c255028b10ac0ea96e2283
Alexandre Nascimento <alexandre.n@outlook.com>
2024-07-27 15:06:55 -0300

    Refactor Product repository methods to handle nullable types.
    Updated repository methods to accept and return nullable types for better null handling and error prevention. Adjusted both `ProductRepository` and `IProductRepository` interfaces accordingly.

--- src/PosTech.MyFood.WebApi/Features/Products/Repositories/IProductRepository.cs
+++ src/PosTech.MyFood.WebApi/Features/Products/Repositories/IProductRepository.cs
@@ -4,9 +4,9 @@ namespace PosTech.MyFood.Features.Products.Repositories;
 
 public interface IProductRepository
 {
-    Task<Product> FindByIdAsync(ProductId id, CancellationToken cancellationToken);
-    Task<List<Product>> FindByCategoryAsync(ProductCategory? category, CancellationToken cancellationToken);
-    Task<Product> CreateAsync(Product product, CancellationToken cancellationToken);
-    Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken);
-    Task DeleteAsync(Product product, CancellationToken cancellationToken);
+    Task<Product?> FindByIdAsync(ProductId id, CancellationToken cancellationToken);
+    Task<List<Product?>> FindByCategoryAsync(ProductCategory? category, CancellationToken cancellationToken);
+    Task<Product?> CreateAsync(Product? product, CancellationToken cancellationToken);
+    Task<Product?> UpdateAsync(Product? product, CancellationToken cancellationToken);
+    Task DeleteAsync(Product? product, CancellationToken cancellationToken);
 }
--- src/PosTech.MyFood.WebApi/Features/Products/Repositories/ProductRepository.cs
+++ src/PosTech.MyFood.WebApi/Features/Products/Repositories/ProductRepository.cs
@@ -6,12 +6,12 @@ namespace PosTech.MyFood.Features.Products.Repositories;
 
 public class ProductRepository(ApplicationDbContext context) : IProductRepository
 {
-    public async Task<Product> FindByIdAsync(ProductId id, CancellationToken cancellationToken)
+    public async Task<Product?> FindByIdAsync(ProductId id, CancellationToken cancellationToken)
     {
         return await context.Products.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
     }
 
-    public Task<List<Product>> FindByCategoryAsync(ProductCategory? category, CancellationToken cancellationToken)
+    public Task<List<Product?>> FindByCategoryAsync(ProductCategory? category, CancellationToken cancellationToken)
     {
         var query = context.Products.AsQueryable();
 
@@ -20,21 +20,21 @@ public class ProductRepository(ApplicationDbContext context) : IProductRepositor
         return query.ToListAsync(cancellationToken);
     }
 
-    public async Task<Product> CreateAsync(Product product, CancellationToken cancellationToken)
+    public async Task<Product?> CreateAsync(Product? product, CancellationToken cancellationToken)
     {
         await context.Products.AddAsync(product, cancellationToken);
         await context.SaveChangesAsync(cancellationToken);
         return product;
     }
 
-    public async Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken)
+    public async Task<Product?> UpdateAsync(Product? product, CancellationToken cancellationToken)
     {
         context.Products.Update(product);
         await context.SaveChangesAsync(cancellationToken);
         return product;
     }
 
-    public Task DeleteAsync(Product product, CancellationToken cancellationToken)
+    public Task DeleteAsync(Product? product, CancellationToken cancellationToken)
     {
         context.Products.Remove(product);
         return context.SaveChangesAsync(cancellationToken);
