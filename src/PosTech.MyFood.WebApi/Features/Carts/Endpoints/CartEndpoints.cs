8ec41fde96fe7aebbc3096b678b74b83675b8e81
Alexandre Nascimento <alexandre.n@outlook.com>
2024-09-24 21:46:58 -0300

    Add checkout endpoint for Cart feature
    Implemented a new endpoint to allow cart checkout using `Checkout.Command`. Updated existing `AddToCart` endpoint to correct variable naming and response paths for consistency.

--- src/PosTech.MyFood.WebApi/Features/Carts/Endpoints/CartEndpoints.cs
+++ src/PosTech.MyFood.WebApi/Features/Carts/Endpoints/CartEndpoints.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using PosTech.MyFood.WebApi.Features.Carts.Commands;
 using PosTech.MyFood.WebApi.Features.Carts.Contracts;
+using PosTech.MyFood.WebApi.Features.Orders.Contracts;
 
 namespace PosTech.MyFood.WebApi.Features.Carts.Endpoints;
 
@@ -12,22 +14,35 @@ public class CartEndpoints : ICarterModule
 
         group.MapPost("/", async (CartRequest request, ISender sender) =>
             {
-                var Command = new AddToCart.Command
+                var command = new AddItensToCart.Command
                 {
                     CustomerId = request.CustomerId,
                     ProductId = request.ProductId,
                     Quantity = request.Quantity
                 };
 
-                var result = await sender.Send(Command);
+                var result = await sender.Send(command);
 
                 return result.IsSuccess
-                    ? Results.Created($"/Carts/{result.Value.Id}", result.Value)
+                    ? Results.Created($"/carts/{result.Value.CartId}", result.Value)
                     : result.ToProblemDetails();
             })
             .WithName("AddToCart")
             .Accepts<CartRequest>("application/json")
             .Produces<CartResponse>(201)
+            .WithTags("Carts")
+            .WithOpenApi();
+
+        group.MapPost("/checkout",
+                async ([FromQuery] Guid cartId, ISender sender) =>
+                {
+                    var result = await sender.Send(new Checkout.Command { CartId = cartId });
+                    return result.IsSuccess
+                        ? Results.Created($"/order/{result.Value.CartId}", result.Value)
+                        : result.ToProblemDetails();
+                })
+            .WithName("Checkout")
+            .Produces<CheckoutResponse>(201)
             .WithTags("Carts")
             .WithOpenApi();
     }
