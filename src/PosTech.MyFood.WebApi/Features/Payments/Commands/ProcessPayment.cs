5888a259f25ece18ee76cb503744a6113aed8c35
Alexandre Nascimento <alexandre.n@outlook.com>
2024-09-24 21:51:36 -0300

    Add ProcessPayment command to handle payment processing
    This commit introduces a new `ProcessPayment` command to manage payment transactions. It includes a command class with properties for transaction ID, status, and amount, and a handler that processes payment notifications using the `IPaymentService`.

---
+++ src/PosTech.MyFood.WebApi/Features/Payments/Commands/ProcessPayment.cs
@@ -0,0 +1,40 @@
+using PosTech.MyFood.WebApi.Features.Payments.Contracts;
+using PosTech.MyFood.WebApi.Features.Payments.Emun;
+using PosTech.MyFood.WebApi.Features.Payments.Notifications;
+using PosTech.MyFood.WebApi.Features.Payments.Services;
+
+namespace PosTech.MyFood.WebApi.Features.Payments.Commands;
+
+public class ProcessPayment
+{
+    public class Command : IRequest<Result<PaymentStatusResponse>>
+    {
+        public string TransactionId { get; set; }
+        public PaymentStatus Status { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+
+    public class Handler(IPaymentService paymentService) : IRequestHandler<Command, Result<PaymentStatusResponse>>
+    {
+        public async Task<Result<PaymentStatusResponse>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var notification = new PaymentNotification
+            {
+                TransactionId = request.TransactionId,
+                Status = request.Status,
+                Amount = request.Amount
+            };
+
+            var result = await paymentService.ProcessPaymentNotificationAsync(notification);
+
+            return result.IsSuccess
+                ? Result.Success(new PaymentStatusResponse
+                {
+                    TransactionId = request.TransactionId,
+                    Status = request.Status
+                })
+                : Result.Failure<PaymentStatusResponse>(result.Error);
+        }
+    }
+}
