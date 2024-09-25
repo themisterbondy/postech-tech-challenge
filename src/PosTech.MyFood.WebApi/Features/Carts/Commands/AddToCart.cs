9c6178f8dcee633fc6feb3112e08ed73b72a65f2
Alexandre Nascimento <alexandre.n@outlook.com>
2024-09-24 21:46:10 -0300

    Rename class and refactor handler dependencies
    Renamed `AddToCart` to `AddItensToCart` for better clarity. Refactored the handler to directly inject dependencies via constructor parameters, ensuring a cleaner and more maintainable code structure.

--- src/PosTech.MyFood.WebApi/Features/Carts/Commands/AddToCart.cs
+++ src/PosTech.MyFood.WebApi/Features/Carts/Commands/AddToCart.cs
@@ -7,7 +7,7 @@ using PosTech.MyFood.WebApi.Features.Products.Repositories;
 
 namespace PosTech.MyFood.WebApi.Features.Carts.Commands;
 
-public class AddToCart
+public class AddItensToCart
 {
     public class Command : IRequest<Result<CartResponse>>
     {
@@ -27,27 +27,19 @@ public class AddToCart
                         .NotEmpty().WithError(Error.Validation("CPF", "CPF is required."))
                         .Matches("^[0-9]*$").WithError(Error.Validation("CPF", "CPF must contain only numbers."))
                         .Length(11).WithError(Error.Validation("CPF", "CPF must have 11 characters."))
-                        .Must(GlobalValidations.BeAValidCPF).WithMessage("CPF is invalid.");
+                        .Must(GlobalValidations.BeAValidCpf).WithMessage("CPF is invalid.");
                 });
             RuleFor(x => x.ProductId).NotEmpty().WithError(Error.Validation("ProductId", "ProductId is required."));
             RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than 0.");
         }
     }
 
-    public class Handler : IRequestHandler<Command, Result<CartResponse>>
+    public class Handler(ICartService cartService, IProductRepository productRepository)
+        : IRequestHandler<Command, Result<CartResponse>>
     {
-        private readonly ICartService _cartService;
-        private readonly IProductRepository _productRepository;
-
-        public Handler(ICartService cartService, IProductRepository productRepository)
-        {
-            _cartService = cartService;
-            _productRepository = productRepository;
-        }
-
         public async Task<Result<CartResponse>> Handle(Command request, CancellationToken cancellationToken)
         {
-            var product = await _productRepository.FindByIdAsync(new ProductId(request.ProductId), cancellationToken);
+            var product = await productRepository.FindByIdAsync(new ProductId(request.ProductId), cancellationToken);
             if (product == null)
                 return Result.Failure<CartResponse>(Error.NotFound("AddToCart.Handler",
                     $"Product with ID {request.ProductId} not found."));
@@ -60,7 +52,7 @@ public class AddToCart
                 Quantity = request.Quantity
             };
 
-            return await _cartService.AddToCartAsync(request.CustomerId, cartItem);
+            return await cartService.AddToCartAsync(request.CustomerId, cartItem);
         }
     }
 }
