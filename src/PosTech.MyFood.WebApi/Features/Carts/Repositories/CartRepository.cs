ff43c043f6f695d77869a346fbf3805a6787ac4b
Alexandre Nascimento <alexandre.n@outlook.com>
2024-10-01 19:26:55 -0300

    Improve cart repository with item tracking and updates.
    Refactored `ExistsAsync` to include cart items while checking existence, using `AsNoTracking`. Enhanced `UpdateAsync` to handle updates more granularly by checking if items exist before adding or updating. This ensures more efficient and accurate cart operations.

--- src/PosTech.MyFood.WebApi/Features/Carts/Repositories/CartRepository.cs
+++ src/PosTech.MyFood.WebApi/Features/Carts/Repositories/CartRepository.cs
@@ -25,7 +25,10 @@ public class CartRepository(ApplicationDbContext context) : ICartRepository
 
     public async Task<bool> ExistsAsync(CartId cartId)
     {
-        return await context.Carts.AnyAsync(c => c.Id == cartId);
+        return await context.Carts
+            .AsNoTracking()
+            .Include(c => c.Items)
+            .AnyAsync(c => c.Id == cartId);
     }
 
     public async Task AddAsync(Cart cart)
@@ -36,13 +39,44 @@ public class CartRepository(ApplicationDbContext context) : ICartRepository
 
     public async Task UpdateAsync(Cart cart)
     {
-        context.Carts.Update(cart);
+        var cartInContext = await context.Carts
+            .Include(c => c.Items)
+            .FirstOrDefaultAsync(c => c.Id == cart.Id);
+
+        if (cartInContext == null)
+        {
+            // Se o carrinho não estiver sendo rastreado, anexe-o e marque como modificado
+            context.Attach(cart);
+            context.Entry(cart).State = EntityState.Modified;
+        }
+        else
+        {
+            // Se o carrinho já está sendo rastreado, atualize apenas os itens
+            foreach (var item in cart.Items)
+            {
+                var existingItem = cartInContext.Items.FirstOrDefault(i => i.Id == item.Id);
+                if (existingItem != null)
+                {
+                    // Se o item já existe, atualize-o
+                    context.Entry(existingItem).CurrentValues.SetValues(item);
+                    context.Entry(existingItem).State = EntityState.Modified;
+                }
+                else
+                {
+                    // Se o item é novo, adicione-o
+                    cartInContext.Items.Add(item);
+                    context.Entry(item).State = EntityState.Added;
+                }
+            }
+        }
+
         await context.SaveChangesAsync();
     }
 
     public async Task DeleteUnpaidCartsOlderThanAsync(DateTime threshold)
     {
         var cartsToDelete = await context.Carts
+            .AsNoTracking()
             .Where(c => c.CreatedAt < threshold
                         && (c.PaymentStatus == PaymentStatus.NotStarted || c.PaymentStatus == PaymentStatus.Pending))
             .ToListAsync();
